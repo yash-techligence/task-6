c2001accc82dc3ebab236b7ad49d030d5f275f58
harshal-techligence <harshal.techligence@gmail.com>
2026-05-23 18:46:22 +0530

    feat: leaderboard and submit APIs completed and tested

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "Harshal@2005",
   database: "quiz_platform",
 });
 

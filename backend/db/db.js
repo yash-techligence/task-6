6b91faf23bd906594c9efb49a76bb879fff4b634
navneetchinchole04 <navneetchinchole04@gmail.com>
2026-05-23 21:34:51 +0530

    Added results API and completed backend integration

--- backend/db/db.js
+++ backend/db/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Harshal@2005",
+  password: "",
   database: "quiz_platform",
 });
 
--- backend/routes/submit.js
+++ backend/routes/submit.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { submitQuiz } = require("../controllers/submitController");
-const verifyToken = require("../middleware/verifyToken");
+const verifyToken = require("../middleware/authMiddleware");
 
 router.post("/", verifyToken, submitQuiz);
 

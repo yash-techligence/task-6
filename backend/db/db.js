b64eff0e2388308de772ecbf642d17dce54af9cd
navneetchinchole04 <navneetchinchole04@gmail.com>
2026-05-25 01:00:42 +0530

    fixed mysql connection

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,11 +1,6 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "quiz_platform",
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect((err) => {
   if (err) {
